007533ab5571103832cd04489b8e15950f99b2da
mohamadgufran <gufranmohamad751@gmail.com>
2026-04-06 14:11:05 +1000

    first commit
    sdf

---
+++ project 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = " Mohamad Gufran ";
+        string address = "Sydney, Australia";
+
+        Console.WriteLine("My Details");
+        Console.WriteLine("-----------");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Address: " + address);
+
+        Console.ReadLine(); // keeps console open
+    }
+}
